2dad9013c7c97d038065f5832fda9844981c3e84
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-04-02 12:44:59 -0600

    Left/ Right Movement with basic block

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from os import listdir
 from os.path import isfile, join
 
 pygame.init()
-pygame.display.set_caption("Maggie's Platformer!")
+pygame.display.set_caption("Maggie's Platform Game!")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
@@ -16,9 +15,75 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, self.rect)
+
+    # self.jumping = pygame.
+
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+
+def draw(window, background, bg_image, player):
+    for tile in background:
+        window.blit(bg_image, tile)
+    player.draw(window)
+    pygame.display.update()
+
+
+def handle_move(player):
+    # Player only moves when key pressed
+    player.x_vel = 0
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
+
 def main(window):
     clock = pygame.time.Clock()
-
+    background, bg_image = get_background("Pink.png")
+    player = Player(100, 100, 50, 50)
     run = True
     while run:
         clock.tick(FPS)
@@ -27,6 +92,10 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background, bg_image, player)
+
     pygame.quit()
     quit()
 
